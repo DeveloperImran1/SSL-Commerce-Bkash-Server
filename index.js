6d40ffa0e4a12d9335b9fab77bb36f46b438c0f0
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-07-17 18:14:07 +0600

    credential private

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://ih9066588:wyZb4giNr3n7v8hI@cluster0.mr9mnat.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mr9mnat.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
